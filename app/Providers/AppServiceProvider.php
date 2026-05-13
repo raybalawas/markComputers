c9d30a5c281c32c938f201fadce5c8dff556b1c4
Rahul Yadav <158089272+raybalawas@users.noreply.github.com>
2026-05-13 11:12:38 +0000

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
     }
