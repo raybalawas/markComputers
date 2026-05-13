e879be715386d577534b3d20ba79954c37e7693c
Rahul Yadav <158089272+raybalawas@users.noreply.github.com>
2026-05-13 11:53:51 +0000

    n

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+     public function boot(): void
     {
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
